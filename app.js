572055265d2204aa4620abeb71d9e01d69f5344c
vladimir <vladi2909@outlook.com>
2021-04-04 16:58:20 +0300

    fix: delete tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -34,16 +34,20 @@ var createNewTaskElement=function(taskString){
 
     label.innerText=taskString;
     label.className='task';
+    label.classList.add('main__label');
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.classList.add('main__input', 'main__input_checkbox');
     editInput.type="text";
     editInput.className="task";
+    editInput.classList.add('main__input', 'main__input_text');
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.classList.add("main__button", "main__button_edit");
 
     deleteButton.classList.add("main__button", "main__button_delete");
+    deleteButtonImg.className='icon';
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -64,6 +68,7 @@ var addTask=function(){
     //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
+    listItem.classList.add('incomplete__item');
 
     //Append listItem to incompleteTaskHolder
     incompleteTaskHolder.appendChild(listItem);
@@ -121,6 +126,8 @@ var taskCompleted=function(){
 
     //Append the task list item to the #completed
     var listItem=this.parentNode;
+    listItem.classList.remove('incomplete__item');
+    listItem.classList.add('completed__item');
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
 
@@ -133,6 +140,8 @@ var taskIncomplete=function(){
     //When the checkbox is unchecked
     //Append the task list item to the #incomplete
     var listItem=this.parentNode;
+    listItem.classList.remove('completed__item');
+    listItem.classList.add('incomplete__item');
     incompleteTaskHolder.appendChild(listItem);
     bindTaskEvents(listItem,taskCompleted);
 }
